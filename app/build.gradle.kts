6824cede3c6d5ae4c2cbbd4bda7ea0404ce81bcd
XxZman <noreply@github.com>
2026-05-20 04:17:30 -0300

    bump version to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.futtv.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
     signingConfigs {
         create("release") {
